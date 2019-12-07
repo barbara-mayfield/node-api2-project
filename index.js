66dc890b356020f47b6d1195f6060fdd33b1d9af
Barbara Mayfield <bmayfield1991@gmail.com>
2019-12-06 20:16:13 -0600

    Stretch goal reached

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const postRouter = require("./routers/posts");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
